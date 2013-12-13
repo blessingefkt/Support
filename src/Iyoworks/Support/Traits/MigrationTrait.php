a0019d5f13454c4dc2cf74c6b92f7ba877dbbc54
Blessing Osakue <iyoworks@gmail.com>
2013-12-12 18:01:10 -0600

    return fluent object from foreign setters
    Signed-off-by: Blessing Osakue <iyoworks@gmail.com>

--- src/Iyoworks/Support/Traits/MigrationTrait.php
+++ src/Iyoworks/Support/Traits/MigrationTrait.php
@@ -1,34 +1,57 @@
 <?php namespace Iyoworks\Support\Traits;
 
+use \Illuminate\Support\Fluent;
+
 trait MigrationTrait {
 
-	public function uid($table, $len = null){
-		return $table->string('uid', $len ?: 36)->unique();
-	}
+    /**
+     * @param $table
+     * @param null $len
+     * @return Fluent
+     */
+    public function uid($table, $len = null){
+        return $table->string('uid', $len ?: 100)->unique();
+    }
 
-	public function metaCols($table, $len = null)
-	{
-		$table->timestamps();
-		$this->uid($table, $len);
-	}
+    public function metaCols($table, $len = null)
+    {
+        $table->timestamps();
+        $this->uid($table, $len);
+    }
 
-	public function foreignId($table, $col, $otherTable = null, $col2 = 'id'){
-		$table->unsignedInteger($col)->nullable();
-		
-		if($otherTable)
-		 	$table->foreign($col)->references($col2)->on($otherTable);
-	}
+    /**
+     * @param $table
+     * @param $col
+     * @param null $otherTable
+     * @param string $col2
+     * @return Fluent
+     */
+    public function foreignId($table, $col, $otherTable = null, $col2 = 'id'){
+        $fluent = $table->unsignedInteger($col)->nullable();
 
-	public function foreignString($table, $col, $otherTable = null, $col2 = null){
-		$table->string($col);
-		
-		if($otherTable)
-		 	$table->foreign($col)->references($col2)->on($otherTable);
-	}
+        if($otherTable)
+            $fluent = $table->foreign($col)->references($col2)->on($otherTable);
+        return $fluent;
+    }
 
-	public function morphs($table, $name)
-	{
-		$table->integer("{$name}_id");
-		$table->string("{$name}_type");
-	}
+    /**
+     * @param $table
+     * @param $col
+     * @param null $otherTable
+     * @param string $col2
+     * @return Fluent
+     */
+    public function foreignString($table, $col, $otherTable = null, $col2 = null){
+        $fluent = $table->string($col);
+
+        if($otherTable)
+            $fluent = $table->foreign($col)->references($col2)->on($otherTable);
+        return $fluent;
+    }
+
+    public function morphs($table, $name)
+    {
+        $table->integer("{$name}_id");
+        $table->string("{$name}_type");
+    }
 }
